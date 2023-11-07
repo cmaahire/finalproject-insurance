a0a0c2e89f1206f907645981520867f0d0db78e3
Seema Ahire <sahire@apple.com>
2023-11-07 13:57:49 -0800

    Test class updated

--- src/test/java/com/project/staragile/insureme/TestContact.java
+++ src/test/java/com/project/staragile/insureme/TestContact.java
@@ -6,81 +6,58 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestContact {
     @Test
-    void verifymessage1() throws InterruptedException {
-
-//        String urlToCheck= "http://54.183.184.195:8081/contact.html";
-//	String urlToCheck= "https://www.facebook.com";
-//        boolean isURLReachable = checkURLReachability(urlToCheck);
-//
-//        if(isURLReachable) {
+    void verifymessage1() {
             System.out.println("Regression test started for ContactUS page");
-
             String expected = "Message Sent";
 
+            //Set up ChromeDriver
             WebDriverManager.chromedriver().setup();
-            Thread.sleep(5000);
+
+            //Setup Chrome options for headless test
             ChromeOptions chromeoptions = new ChromeOptions();
             chromeoptions.addArguments("--headless");
-            WebDriver driver = new ChromeDriver(chromeoptions);
-            Thread.sleep(5000);
 
+            //Initialize Webdriver
+            WebDriver driver = new ChromeDriver(chromeoptions);
+
+            //Open Test Server URL
             driver.get("http://54.183.184.195:8081/contact.html");
-            Thread.sleep(5000);
             driver.manage().window().maximize();
+
+            //Locate name field and enter name
             driver.findElement(By.id("inputName")).sendKeys("john");
             System.out.println("Entered Name");
 
+            //Locate number field and enter number
             driver.findElement(By.id("inputNumber")).sendKeys("8888888888");
             System.out.println("Entered Mobile number");
 
+            //Locate email field and enter email
             driver.findElement(By.id("inputMail")).sendKeys("test@test.com");
             System.out.println("Entered Email address");
 
+            //Locate message field and enter message
             driver.findElement(By.id("inputMessage")).sendKeys("Test selenium");
             System.out.println("Entered message");
 
+            //Locate Send button and click
             driver.findElement(By.id("my-button")).click();
             System.out.println("Clicked on Send button");
 
+             //Get response text after clicking send button and save
             String actualMessage = driver.findElement(By.id("response")).getText();
 
+            //Verify if actual and expected text are matching
             assertEquals(expected, actualMessage);
 
             System.out.println("Regression test completed for ContactUs Page");
             driver.close();
-
-//        } else {
-//            Assume.assumeTrue("Skipping test because URL unreachable", false);
-//        }
+    }
 
     }
 
-    private boolean checkURLReachability(String urlString) {
-        try{
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            connection.setRequestMethod("HEAD");
-            connection.setConnectTimeout(5000);
-            int responseCode =connection.getResponseCode();
-            return (responseCode == HttpURLConnection.HTTP_OK);
-        }catch(MalformedURLException e){
-            return false;
-        }catch (IOException e){
-            return false;
-        }
-
-
-    }
-
-}
